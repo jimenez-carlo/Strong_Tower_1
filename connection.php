d28897974498267a5f06bfed7dd3257165e91c0d
Carlo Jimenez <jimenez.carlo.llabor@gmail.com>
2022-11-07 10:56:04 +0800

    update_connection & sql

--- connection.php
+++ connection.php
@@ -2,11 +2,7 @@
 $host = "localhost";
 $username= "root";
 $password="";
-$database="strong_db";
+$database="strong_db_1";
 
 $con = mysqli_connect($host, $username, $password);
 mysqli_select_db($con, $database)or die (error_reporting(E_ALL));
-
-
-
-?>
